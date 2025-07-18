89bfc890cdffc0ddf352270d4757128ba658d8ea
Wilson Kao <wilkao036@gmail.com>
2025-07-19 01:48:37 +0800

    設定介面初始

--- src/options/App.tsx
+++ src/options/App.tsx
@@ -1,13 +1,32 @@
+import { Button, Form, InputNumber, Space, Typography } from "antd"
 
-
+const { Title } = Typography;
 
 function App() {
-  console.log('App2')
 
+  const onFinish = (values: { group: number }) => {
+    console.log(values)
+    chrome.storage.sync.set(values,()=>{
+      chrome.runtime.sendMessage({
+        type: 'update-group',
+        group: values.group
+      })
+    })
+  }
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Space direction="vertical" size={16}>
+      <Title>番茄鐘設定</Title>
+      <Form layout="vertical" onFinish={onFinish}>
+        <Form.Item label="番茄鐘組數" name="group">
+          <InputNumber min={1} max={10} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            設定
+          </Button>
+        </Form.Item>
+      </Form>
+    </Space>
   )
 }
 
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,44 +1,54 @@
-import { Button, Card, Progress, Space, Typography } from "antd";
+import {  Card, Typography } from "antd";
 import { useEffect, useState } from "react"
 
 const { Title } = Typography;
 
 function App() {
-  const [ currentTime, setCurrentTime ] = useState('')
+  const [currentTime, setCurrentTime] = useState('')
+  const [group, setGroup] = useState(1)
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentTime(new Date().toLocaleTimeString('zh-TW', {
-        hour12: true,
         hour: '2-digit',
         minute: '2-digit',
-        second: '2-digit'
+        second: '2-digit',
+        hour12: true
       }));
     }, 1000);
     return () => clearInterval(interval)
+
   }, [])
- 
+
+  useEffect(() => {
+    chrome.storage.sync.get(['group'], (result) => {
+      console.log(result)
+      setGroup(result.group || 1)
+    })
+  }, [])
+
   return (
-    <Card 
-    style={{ 
-      background: '#000', 
-      borderRadius: '8px',
-      textAlign: 'center',
-      padding: '12px'
-    }}
-  >
-    <Title 
-      level={2}
+    <Card
       style={{
-        color: '#00ff00',
-        fontFamily: 'Courier New, monospace',
-        margin: 0,
-        fontSize: '24px',
-        letterSpacing: '2px'
+        background: '#000',
+        borderRadius: '8px',
+        textAlign: 'center',
+        padding: '12px'
       }}
     >
-      {currentTime}
-    </Title>
-  </Card>
+      <Title
+        level={2}
+        style={{
+          color: '#00ff00',
+          fontFamily: 'Courier New, monospace',
+          margin: 0,
+          fontSize: '24px',
+          letterSpacing: '2px'
+        }}
+      >
+        {currentTime}
+      </Title>
+      {group && <Title level={4} style={{ color: '#00ff00' }}>組數：{group}</Title>}
+    </Card>
   )
 }
 
